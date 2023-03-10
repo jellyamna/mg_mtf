30abdf3c89563c8b7dad31b23bbdc85bc6c871c7
jelly a <jellyamna@yahoo.com>
2023-03-10 14:07:34 +0700

    ok

--- src/hooks/useDataMg.js
+++ src/hooks/useDataMg.js
@@ -3,16 +3,16 @@ import axios from "axios";
 
 const fetchSuperHeroes = () => {
   //return axios.get("https://mg-api-fu5l.onrender.com/account");
-  //return axios.get("https://www.api.mgcanalis.tech/account");
+  return axios.get("https://www.api.mgcanalis.tech/account");
    //return axios.get("http://localhost:3001/account");
-   return axios.get("http://localhost:4000/account");
+  // return axios.get("http://localhost:4000/account");
 };
 
 export const useDataMg = (onSuccess, onError) => {
   return useQuery("data-mandiri", fetchSuperHeroes, {
     onError,
     onSuccess,
-    refetchInterval: 5000,
+    refetchInterval: 20000,
     refetchIntervalInBackground: true,
     // cacheTime: 1000,
     refetchOnWindowFocus: true,
